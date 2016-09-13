964f542b64408a33cc9e37b4ae5cf0c163a3f985
Tristan Sloughter <t@crashfast.com>
2016-09-13 14:32:13 -0700

    add match on lock pkg hash to deps filtering

--- src/rebar3_hex_pkg.erl
+++ src/rebar3_hex_pkg.erl
@@ -104,7 +104,7 @@ publish(App, State) ->
     {application, _, AppDetails} = rebar3_hex_utils:update_app_src(App, ResolvedVersion),
 
     Deps = rebar_state:get(State, {locks, default}, []),
-    TopLevel = [{N, [{<<"app">>, A}, {<<"optional">>, false}, {<<"requirement">>, V}]} || {A,{pkg,N,V},0} <- Deps],
+    TopLevel = [{N, [{<<"app">>, A}, {<<"optional">>, false}, {<<"requirement">>, V}]} || {A,{pkg,N,V,_},0} <- Deps],
     Excluded = [binary_to_list(N) || {N,{T,_,_},0} <- Deps, T =/= pkg],
 
     case validate_app_details(AppDetails) of
