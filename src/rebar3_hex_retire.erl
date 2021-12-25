df93ea82b94fae85c31b89949b7776085e4725dd
Bryan Paxton <39971740+starbelly@users.noreply.github.com>
2021-12-25 11:37:08 -0600

    Adjust retire to take bare arguments and a message switch (#270)

--- src/rebar3_hex_retire.erl
+++ src/rebar3_hex_retire.erl
@@ -25,43 +25,40 @@ init(State) ->
                                  {namespace, hex},
                                  {bare, true},
                                  {deps, ?DEPS},
-                                 {example, "rebar3 hex retire some_pkg 0.3.0"},
+                                 {example, "rebar3 hex retire some_pkg 0.3.0 invalid --message Clarifying message"},
                                  {short_desc, "Mark a package as deprecated."},
                                  {desc, ""},
-                                 {opts, [{pkg, undefined, undefined, string, "Name of the package to retire."},
-                                         {vsn, undefined, undefined, string, "Version of the package to retire."},
-                                         {reason, undefined, undefined, string, "Reason to retire package."},
-                                         {message, undefined, undefined, string, "Clarifying message for retirement"},
+                                 {opts, [
+                                         {message, $m, "message", string, "Clarifying message for retirement"},
                                          rebar3_hex:repo_opt()]}]),
     State1 = rebar_state:add_provider(State, Provider),
     {ok, State1}.
 
 -spec do(rebar_state:t()) -> {ok, rebar_state:t()} | {error, {?MODULE, rebar3_hex_config:repo_error()}}.
 do(State) ->
-    case rebar3_hex_config:repo(State) of
-        {ok, Repo} ->
-            handle_command(State, Repo);
+        case rebar3_hex_config:repo(State) of
+            {ok, Repo} ->
+                case rebar_state:command_args(State) of
+                    [Pkg, Version, Reason|_] ->
+                        {Opts, _} = rebar_state:command_parsed_args(State),
+                        Message = rebar_utils:to_binary(get_required_or_raise(message, Opts)),
+                        PkgName = rebar_utils:to_binary(Pkg),
+                        VersionBin = rebar_utils:to_binary(Version),
+                        ReasonBin = rebar_utils:to_binary(Reason),
+                        MessageBin = rebar_utils:to_binary(Message),
+                        retire(State, PkgName, VersionBin, Repo, ReasonBin, MessageBin);
+                    _ ->
+                        ?RAISE(bad_command)
+        end;
+
         {error, Reason} ->
-            ?PRV_ERROR(Reason)
+            ?RAISE(Reason)
     end.
-
-handle_command(State, Repo) ->
-    {Args, _} = rebar_state:command_parsed_args(State),
-    Name = get_required_or_raise(pkg, Args),
-    Version = get_required_or_raise(vsn, Args),
-    Reason = get_required_or_raise(reason, Args),
-    Message = get_required_or_raise(message, Args),
-    PkgName = rebar_utils:to_binary(Name),
-    retire(PkgName, rebar_utils:to_binary(Version), Repo,
-           rebar_utils:to_binary(Reason),
-           rebar_utils:to_binary(Message),
-           State).
-
 
 get_required_or_raise(Key, Args) ->
     case rebar3_hex:get_required(Key, Args) of
             {error, Err} ->
-                erlang:error(?PRV_ERROR(Err));
+                ?RAISE(Err);
             Res ->
                 Res
     end.
@@ -91,28 +88,28 @@ format_error({required, reason}) ->
     "retire requires a reason with value of either other, invalid, security, deprecated or renamed";
 format_error({required, message}) ->
     "retire requires a message to clarify the reason for the retirement of the package";
+format_error(bad_command) ->
+    "Invalid arguments, expected one of:\n\n"
+    "rebar3 hex retire PACKAGE VERSION REASON --message MESSAGE\n";
 format_error(Reason) ->
     rebar3_hex_error:format_error(Reason).
 
-retire(PkgName, Version, Repo, RetireReason, RetireMessage, State) ->
+retire(State, PkgName, Version, Repo, RetireReason, RetireMessage) ->
     case rebar3_hex_config:hex_config_write(Repo) of
         {error, no_write_key} ->
-            ?PRV_ERROR({no_write_key, maps:get(name, Repo)});
-
+            ?RAISE({no_write_key, maps:get(name, Repo)});
         {ok, HexConfig} ->
-
             Msg = #{<<"reason">> => RetireReason,
                      <<"message">> => RetireMessage},
-
             case hex_api_release:retire(HexConfig, PkgName, Version, Msg) of
                 {ok, {204, _Headers, _Body}} ->
                     rebar_api:info("Successfully retired package ~ts ~ts", [PkgName, Version]),
                     {ok, State};
                 {ok, {422, _Headers, #{<<"errors">> := Errors, <<"message">> := Message}}} ->
-                    ?PRV_ERROR({validation_errors, Errors, Message});
+                    ?RAISE({validation_errors, Errors, Message});
                 {ok, {Code, _Headers, _Body}} ->
-                    ?PRV_ERROR({api_error, PkgName, Version, rebar3_hex_client:pretty_print_status(Code)});
+                    ?RAISE({api_error, PkgName, Version, rebar3_hex_client:pretty_print_status(Code)});
                 {error, Reason} ->
-                    ?PRV_ERROR({api_error, PkgName, Version, io_lib:format("~p", [Reason])})
+                    ?RAISE({api_error, PkgName, Version, io_lib:format("~p", [Reason])})
             end
     end.
