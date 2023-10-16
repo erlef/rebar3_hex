f9668b05f4841e804ab740f9cb9e6f944fe4dd2f
Bryan Paxton <39971740+starbelly@users.noreply.github.com>
2023-10-15 22:28:46 -0500

    Update src/rebar3_hex_build.erl
    Co-authored-by: Paulo F. Oliveira <paulo.ferraz.oliveira@gmail.com>

--- src/rebar3_hex_build.erl
+++ src/rebar3_hex_build.erl
@@ -437,11 +437,11 @@ include_files(Name, AppDir, AppDetails) ->
 
 maybe_warn_about(_, undefined) -> ok;
 maybe_warn_about(include_files, _) ->
-     Deprecation = "include_files has been deprecated, which will be "
-                        ++ "removed in a later version of rebar3_hex."
-                        ++ "You should now use include_paths instead. See "
-                        ++ "rebar3 help hex build or the docs for "
-                        ++ "rebar3_hex_build for more info.",
+     Deprecation = "include_files has been deprecated, and will be "
+                        ++ "removed in a later version of rebar3_hex. "
+                        ++ "You should use include_paths instead. See "
+                        ++ "\"rebar3 help hex build\" or file \"rebar3_hex_build\"'s "
+                        ++ "docs for more info.",
      rebar_api:warn(Deprecation, []);
 maybe_warn_about(exclude_regexps, _) ->
      Deprecation = "exclude_regexps has been deprecated, and will be "
